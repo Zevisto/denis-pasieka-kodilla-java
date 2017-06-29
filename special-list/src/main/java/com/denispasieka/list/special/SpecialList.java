f07c2a1b256c62e09b0b20c074f63e2833f47b75
Zevisto <zevisto@gmail.com>
2017-06-29 18:43:06 +0200

    Class for Special List created.

---
+++ special-list/src/main/java/com/denispasieka/list/special/SpecialList.java
@@ -0,0 +1,16 @@
+package com.denispasieka.list.special;
+
+/**
+ * Created by zevisto on 29.06.17.
+ */
+public class SpecialList {
+
+    SpecialListElement listHead;
+
+    public void add(String s);
+    public boolean remove(String s);
+    public String get(int n);
+    public int size();
+    public void add (int n, String s);
+
+}
--- special-list/src/main/java/com/denispasieka/list/special/SpecialListElement.java
+++ special-list/src/main/java/com/denispasieka/list/special/SpecialListElement.java
@@ -4,4 +4,9 @@ package com.denispasieka.list.special;
  * Created by zevisto on 29.06.17.
  */
 public class SpecialListElement {
+    
+    String value;
+    SpecialListElement next;
+    SpecialListElement previous;
+
 }
