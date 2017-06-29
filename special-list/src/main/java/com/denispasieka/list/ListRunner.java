84a417c1205015ea4453bf1e9c258d67b8a61755
Zevisto <zevisto@gmail.com>
2017-06-29 18:29:45 +0200

    special list init

---
+++ special-list/src/main/java/com/denispasieka/list/ListRunner.java
@@ -0,0 +1,12 @@
+package com.denispasieka.list;
+
+/**
+ * Created by zevisto on 29.06.17.
+ */
+public class ListRunner {
+
+    public static void main(String[] args){
+        System.out.println("Hello in the list module");
+    };
+
+}
---
+++ special-list/src/main/java/com/denispasieka/list/special/SpecialListElement.java
@@ -0,0 +1,7 @@
+package com.denispasieka.list.special;
+
+/**
+ * Created by zevisto on 29.06.17.
+ */
+public class SpecialListElement {
+}
