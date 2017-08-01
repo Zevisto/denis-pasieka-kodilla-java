be3e8333e69f44d4f7139a4725dd02b74b72a34e
Zevisto <zevisto@gmail.com>
2017-08-01 12:58:59 +0200

    Count Stat Test finished.

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CountStats.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CountStats.java
@@ -9,12 +9,12 @@ public class CountStats {
     int averageCommentsPerPost = 0;
 
     public void showStatistics() {
-        System.out.println("Number of users : " + usersCount + "." +
-                "Number of posts : " + postsCount + "." +
-                "Number of comments : " + commentsCount + "." +
-                "Average number of posts per user : " + averagePostsPerUser + "." +
-                "Average number of comments per user : " + averageCommentsPerUser + "." +
-                "Average number of comments per post : " + averageCommentsPerPost + ".");
+        System.out.println("Number of users : " + usersCount + ".\n" +
+                "Number of posts : " + postsCount + ".\n" +
+                "Number of comments : " + commentsCount + ".\n" +
+                "Average number of posts per user : " + averagePostsPerUser + ".\n" +
+                "Average number of comments per user : " + averageCommentsPerUser + ".\n" +
+                "Average number of comments per post : " + averageCommentsPerPost + ".\n");
     }
     public void calculateAdvStatistics(Statistics statistics) {
         usersCount = statistics.usersNames().size();
@@ -23,5 +23,6 @@ public class CountStats {
         averagePostsPerUser = postsCount / usersCount;
         averageCommentsPerUser = commentsCount / usersCount;
         averageCommentsPerPost = postsCount / commentsCount;
+
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CountStatsTest.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CountStatsTest.java
@@ -1,28 +1,32 @@
 package com.kodilla.testing.forum.statistics;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+
 public class CountStatsTest {
-    CountStats testObject = new CountStats();
     Statistics statisticsMock = Mockito.mock(Statistics.class);
+    CountStats testObject = new CountStats();
     @Test
     public void calculateAdvStatistics() {
+        ArrayList<String> myMockList = new ArrayList<>();
         for(int x = 0; x < 100; x++) {
-            statisticsMock.usersNames().add(x, "User.");
+            myMockList.add(x, "User.");
         }
-        int userListSize = statisticsMock.usersNames().size();
-        Mockito.when(statisticsMock.usersNames().size()).thenReturn(userListSize);
+        Mockito.when(statisticsMock.usersNames()).thenReturn(myMockList);
         Mockito.when(statisticsMock.commentsCount()).thenReturn(100);
         Mockito.when(statisticsMock.postsCount()).thenReturn(1000);
         testObject.calculateAdvStatistics(statisticsMock);
         testObject.showStatistics();
+        Assert.assertTrue(statisticsMock.usersNames().size() == 100);
     }
     @Before
     public void before() {
-        System.out.println("Starting test.");
+        System.out.println("Starting test.\n");
     }
     @After
     public void after() {
